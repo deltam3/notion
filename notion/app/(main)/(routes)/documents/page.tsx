251d13b9a05a09ebc106d8a89cbcc440c3c0db35
deltam3 <practicect@proton.me>
2024-04-07 13:26:46 +0900

    user settings related component

---
+++ notion/app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+
+const DocumentsPage = () => {
+  const router = useRouter();
+  const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
+
+    toast.promise(promise, {
+      loading: "새로운 노트를 생성중",
+      success: "새로운 노트를 생성했습니다.!",
+      error: "노트 생성 실패!",
+    });
+  };
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        {user?.firstName}님 &apos;s 노션에 환영합니다.
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        노트 생성
+      </Button>
+    </div>
+  );
+};
+
+export default DocumentsPage;
