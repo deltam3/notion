73e24644113a91c2149846c83cda8dcfd5c90c37
deltam3 <practicect@proton.me>
2024-04-07 14:08:45 +0900

    npm i cmdk and radix

---
+++ notion/app/(main)/_components/banner.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+
+interface BannerProps {
+  documentId: Id<"documents">;
+}
+
+export const Banner = ({ documentId }: BannerProps) => {
+  const router = useRouter();
+
+  const remove = useMutation(api.documents.remove);
+  const restore = useMutation(api.documents.restore);
+
+  const onRemove = () => {
+    const promise = remove({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "노트 삭제중...",
+      success: "노트 삭제 완료",
+      error: "노트 삭제 실패",
+    });
+
+    router.push("/documents");
+  };
+
+  const onRestore = () => {
+    const promise = restore({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "삭제된 노트 복구중",
+      success: "노트 복구 완료",
+      error: "노트 복구 실패",
+    });
+  };
+
+  return (
+    <div className="w-full bg-rose-500 text-center text-sm p-2 text-white flex items-center gap-x-2 justify-center">
+      <p>이 페이지는 쓰레기통에 있습니다.</p>
+      <Button
+        size="sm"
+        onClick={onRestore}
+        variant="outline"
+        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
+      >
+        페이지 복구
+      </Button>
+      <ConfirmModal onConfirm={onRemove}>
+        <Button
+          size="sm"
+          variant="outline"
+          className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
+        >
+          영구 삭제
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
---
+++ notion/app/(main)/_components/menu.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { MoreHorizontal, Trash } from "lucide-react";
+
+import { Id } from "@/convex/_generated/dataModel";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface MenuProps {
+  documentId: Id<"documents">;
+}
+
+export const Menu = ({ documentId }: MenuProps) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = () => {
+    const promise = archive({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "쓰레기통으로 이동중입니다...",
+      success: "노트가 쓰레기통으로 이동했습니다.",
+      error: "에러가 발생했습니다.",
+    });
+
+    router.push("/documents");
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="sm" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-60"
+        align="end"
+        alignOffset={8}
+        forceMount
+      >
+        <DropdownMenuItem onClick={onArchive}>
+          <Trash className="h-4 w-4 mr-2" />
+          삭제
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className="text-xs text-muted-foreground p-2">
+          마지막으로 편집한 유저: {user?.fullName}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+  return <Skeleton className="h-10 w-10" />;
+};
